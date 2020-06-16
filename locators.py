ea12ae1bf74366b133ebcfaad40705c8d6496817
KirillKurganov <kurganovkirill2512@gmail.com>
2020-06-16 14:43:16 +0300

    update file

--- locators.py
+++ locators.py
@@ -1,8 +1,5 @@
 from selenium.webdriver.common.by import By
 
-class MainPageLocators():
-    LOGIN_LINK=(By.CSS_SELECTOR, '#login_link')
-
 class LoginPageLocators():
     LOGIN_FORM=(By.CSS_SELECTOR, '#login_form')
     REGISTER_FORM=(By.CSS_SELECTOR, '#register_form')
@@ -13,5 +10,20 @@ class ProductPageLocators():
     PRICE_BASKET=(By.CSS_SELECTOR,'.alert-info .alertinner strong')
     PRICE_PRODUCT=(By.CSS_SELECTOR,'.product_main .price_color')
     MESSAGE=(By.CSS_SELECTOR,'div.alertinner')
+
+class ProductLocators():
+    SUCCESS_MESSAGE=(By.CSS_SELECTOR,'div.alertinner')
+
+class BasePageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
+
+class BasketPageLocators():
+    BASKET_BUTTON=(By.CSS_SELECTOR, '.btn-group .btn.btn-default')
+    MESSAGE_FROM_BASKET=(By.CSS_SELECTOR,'#content_inner p')
+    BASKET_FULL=(By.CSS_SELECTOR,'#basket_formset')
+
+
+
 
 
